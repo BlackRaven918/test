c7a5d2a6e44037ed28733d2b481b543341dc9cb9
Jasper Vanhoutte <jasper.vanhoutte@hotmail.com>
2025-11-27 17:54:46 +0100

    minder shit

--- ltitool.py
+++ ltitool.py
@@ -1,13 +1,4 @@
-from flask import Flask, request, redirect, session, render_template_string
-from logging.config import dictConfig
-from random import randint
-import os
-import urllib.parse
-import time
-import hmac
-import hashlib
-import base64
-from urllib.parse import quote
+from flask import Flask
 
 
 app = Flask(__name__)
